74435c54b2e68929a046919277309564544e3290
Mateusz Woźniak <matisiek11@gmail.com>
2023-03-15 11:48:40 +0100

    Initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,71 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <input placeholder="Name" v-model="name">
+    <input placeholder="Port" v-model="port" type="number">
+    <input placeholder="Host" v-model="host">
+    <br/>
+    <br/>
+    <textarea readonly>{{ code }}</textarea>
   </div>
 </template>
 
 <script>
+var template = `apiVersion: v1
+kind: Endpoints
+metadata:
+  name: {name}
+subsets:
+  - addresses:
+      - ip: 95.111.255.92
+    ports:
+      - port: {port}
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: {name}
+spec:
+  ports:
+    - protocol: TCP
+      port: {port}
+      targetPort: {port}
+---
+apiVersion: extensions/v1beta1
+kind: Ingress
+metadata:
+  name: {name}
+  annotations:
+    kubernetes.io/ingress.class: traefik
+spec:
+  rules:
+    - host: {host}
+      http:
+        paths:
+          - path: /
+            backend:
+              serviceName: {name}
+              servicePort: {port}`;
+
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return {
+      name: '',
+      port: '',
+      host: '',
+    }
+  },
+  computed:{
+    code(){
+      return template.replace(/{name}/g, this.name).replace(/{port}/g, this.port).replace(/{host}/g, this.host);
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+textarea {
+  width: 100%;
+  height: 800px;
 }
 </style>
